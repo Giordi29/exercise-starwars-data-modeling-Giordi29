7a371c48ee229ac2d0e7d495cbb330edbf061564
Giordi29 <157144092+Giordi29@users.noreply.github.com>
2024-07-17 10:20:48 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -7,26 +7,71 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    surname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Vehicles(Base):
+    __tablename__ = 'Vehicles'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    model = Column(String(250), nullable=False)
+    vehicle_name = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
+
+class Fave_Vehicles(Base):
+    __tablename__ = 'Fave_Vehicles'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.id"), nullable=False)
+    vehicle_name = Column(Integer, ForeignKey("Vehicles.id"), nullable=False)
+
+class Characters(Base):
+    __tablename__ = 'Characters'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(250), nullable=False)
+
+class Fave_chars(Base):
+    __tablename__ = 'Fave_chars'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.id"), nullable=False)
+    character_id = Column(Integer, ForeignKey("Characters.id"), nullable=False)
+
+class Planets(Base):
+    __tablename__ = 'Planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.id"), nullable=False)
+    character_id = Column(Integer, ForeignKey("Characters.id"), nullable=False)
+
+class Fave_plts(Base):
+    __tablename__ = 'Fave_plts'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.id"), nullable=False)
+    planet_id = Column(Integer, ForeignKey("Planets.id"), nullable=False)
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
+
+
+
